74bef6c8f8fa935a543eb67d84d89c97d2558690
Messquerade <annac.klingberg@gmail.com>
2021-08-17 16:16:24 -0700

    add censor function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
 
 function wordCounter(text) {
@@ -15,7 +21,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -28,3 +34,68 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+function censor(text) {
+  text = text.replaceAll("zoinks", "#$*&");
+  text = text.replaceAll("muppeteer", "%^**&");
+  text = text.replaceAll("biffaroni", "%$#^*");
+  text = text.replaceAll("loopdaloop", "&%^&#%$@");
+  return text;
+}
+
+// function mostFrequentWords(text) {
+//   if (text.trim().length === 0){
+//     return "";
+//   }
+//   let wordArray = text.split(" ");
+//   wordArray = wordArray.sort();
+//   let wordcount = 1;
+//   wordArray.forEach(function(element, index) {
+//     wordArray.forEach(function(element))
+//   if (element === wordArray[index + 1]) {
+//     wordcount ++;
+//   } else if (element !== wordArray[index + 1] && wordcount !== 1) {
+//     let frequentword = [element, wordcount];
+//   }
+//    return "no repeated words";
+// });
+// };
+
+
+
+
+
+// UI Logic
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  const censoredText = censor(text);
+  let textArray = censoredText.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
+    // $("#frequent-words").html(mostFrequentWords(passage));
+  });
+});
